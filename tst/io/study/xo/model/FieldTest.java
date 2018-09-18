f3383efaca4f65a97a812c3ab3551b5027c64769
Alex Godyna <alexey.godyna@gmail.com>
2018-09-18 22:37:09 +0300

    already occupied point test

--- tst/io/study/xo/model/FieldTest.java
+++ tst/io/study/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.study.xo.model;
 
+import io.study.xo.model.exeptions.AlredyOccupiedException;
 import io.study.xo.model.exeptions.InvalidPointException;
 import org.junit.Test;
 
@@ -83,6 +84,20 @@ public class FieldTest {
             final Figure actualFigure = field.getFigure(inputPoint);
             fail();
         } catch (final InvalidPointException e){}
+    }
+
+    @Test
+    public void testFigureWhenAlreadyOccupied() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlredyOccupiedException e){}
     }
 
 }
