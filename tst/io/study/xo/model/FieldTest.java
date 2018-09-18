87597a02cbbf173039293f2ef6bfff8e940d96ab
Alex Godyna <alexey.godyna@gmail.com>
2018-09-18 22:28:40 +0300

    remaining invalidPointException

--- tst/io/study/xo/model/FieldTest.java
+++ tst/io/study/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.study.xo.model;
 
+import io.study.xo.model.exeptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -28,6 +29,38 @@ public class FieldTest {
         final Figure expectedFigure = Figure.O;
 
         assertEquals(expectedFigure, actualFigure);
+    }
+
+    @Test
+    public void testFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testFigureWhenXLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){}
+    }
+
+    @Test
+    public void testFigureWhenYLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e){}
     }
 
 }
