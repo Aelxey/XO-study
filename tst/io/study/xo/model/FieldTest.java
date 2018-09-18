62e010ea2c4c17c1843f7cba0f59571ad2cde73d
Alex Godyna <alexey.godyna@gmail.com>
2018-09-18 23:06:06 +0300

    FieldTest have been fixed (deleted testFigureWhenAlreadyOccupied test)

--- tst/io/study/xo/model/FieldTest.java
+++ tst/io/study/xo/model/FieldTest.java
@@ -86,18 +86,5 @@ public class FieldTest {
         } catch (final InvalidPointException e){}
     }
 
-    @Test
-    public void testFigureWhenAlreadyOccupied() throws Exception {
-        final Field field = new Field();
-        final Point inputPoint = new Point(0,0);
-        final Figure inputFigure = Figure.O;
-
-        field.setFigure(inputPoint, inputFigure);
-
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail();
-        } catch (final AlredyOccupiedException e){}
-    }
 
 }
